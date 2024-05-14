924b77e6bfccc92758ad02e9e28ea514143f2cd1
waapsdeveloper <124657993+HamzaShahidWaaps@users.noreply.github.com>
2024-05-14 17:46:16 +0500

    gfhgfh

--- src/app/components/sd-image-box/sd-image-box.component.ts
+++ src/app/components/sd-image-box/sd-image-box.component.ts
@@ -1,29 +1,20 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { NetworkService } from 'src/app/services/network.service';
-
 @Component({
   selector: 'app-sd-image-box',
   templateUrl: './sd-image-box.component.html',
   styleUrls: ['./sd-image-box.component.scss'],
 })
 export class SdImageBoxComponent implements OnInit {
-
   profilePhoto: SafeUrl | undefined;
   photoId: SafeUrl | undefined;
-
   @Output('openGallery') openGallery: EventEmitter<any> = new EventEmitter<any>();
   @Output('updateImage') updateImage: EventEmitter<any> = new EventEmitter<any>();
   @Output('updatePhotoId') updatePhotoId: EventEmitter<any> = new EventEmitter<any>();
-
-
-
-
   constructor(private network: NetworkService) {
-
     this.initialize()
   }
-
   ngOnInit() {
   }
   initialize() {
@@ -32,23 +23,17 @@ export class SdImageBoxComponent implements OnInit {
     this.profilePhoto = user.image;
     this.photoId = user.teacher.photo_id;
   }
-
-
   onProfileSelected(event: any) {
     const file: File = event.target.files[0];
     const reader = new FileReader();
     reader.onload = async () => {
       const pmi = reader.result as string;
       console.log(pmi);
-
-      // send it to API for upload
       let user = JSON.parse(localStorage.getItem('user'));
-
       let obj = {
         user_id: user.id,
         image: pmi
       }
-
       const res = await this.network.postProfileImage(obj)
       console.log(res);
       this.profilePhoto = res.result.image;
@@ -63,25 +48,17 @@ export class SdImageBoxComponent implements OnInit {
     reader.onload = async () => {
       const pmil = reader.result as string;
       console.log(this.photoId, "dfsfsf");
-
-      // send it to API for upload
       let user = JSON.parse(localStorage.getItem('user'));
-
       let obj = {
         user_id: user.id,
         image: pmil
       }
       console.log("test", obj);
-
-      // return
-
       const res = await this.network.postPhotoIdImage(obj)
       console.log(res);
       this.photoId = res.result.image;
       this.updatePhotoId.emit(res.image)
-
     };
     reader.readAsDataURL(file);
   }
-
 }
