2cc7bda8e7590f261d15dc580a8b9a2addf94ecf
waapsdeveloper <124657993+HamzaShahidWaaps@users.noreply.github.com>
2024-07-08 20:31:19 +0500

    hj

--- src/app/components/sd-textarea-about/sd-textarea-about.component.ts
+++ src/app/components/sd-textarea-about/sd-textarea-about.component.ts
@@ -37,18 +37,21 @@ export class SdTextareaAboutComponent implements OnInit {
     this.events.subscribe('teacher-course-first-screen-submit-call', (formData: any) => {
 
       let v = formData[this.key];
-      if (!v || v == 'description') {
-        this.isRequired = true;
-        setTimeout(() => {
-          this.isRequired = false;
-        }, 5000);
-      }
-      if (v && v.length < 250) {
-        this.isRequired = true;
-        this.errorText = 'The Detail field should have minimum 250 characters'
-        setTimeout(() => {
-          this.isRequired = false;
-        }, 5000);
+      console.log(v);
+      if (this.key == 'description') {
+        if (!v || v == '') {
+          this.isRequired = true;
+          setTimeout(() => {
+            this.isRequired = false;
+          }, 5000);
+        }
+        if (v  && v.length < 250) {
+          this.isRequired = true;
+          this.errorText = 'The Detail field should have minimum 250 characters'
+          setTimeout(() => {
+            this.isRequired = false;
+          }, 5000);
+        }
       }
     }, false);
 
