4a7fdaf02f834c9f15af28faefdedb2c24833074
waapsdeveloper <124657993+HamzaShahidWaaps@users.noreply.github.com>
2024-07-18 17:33:24 +0500

    rtrt

--- src/app/components/my-courses/course-list/course-list.component.ts
+++ src/app/components/my-courses/course-list/course-list.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, Input, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BasePage } from 'src/app/base-page/base-page';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss'],
 })
-export class CourseListComponent  implements OnInit {
-
-  constructor() { }
-
+export class CourseListComponent extends BasePage implements OnInit {
+  @Input() list: any[] = [];
+  course
+  @Input() item
+  @ViewChild('slides', { static: false }) slides: any;
+  constructor(private _sanitizer: DomSanitizer, injector: Injector) {
+    super(injector)
+  }
   ngOnInit() {}
+
+  oepnDeatils(item) {
+
+    const params = {
+      id: item.id,
+      backUrl: '/tabs/courses'
+    }
+    this.nav.push('/tabs/course-detail', params)
+
+  }
 
 }
--- src/app/components/my-courses/my-courses.component.ts
+++ src/app/components/my-courses/my-courses.component.ts
@@ -1,14 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BasePage } from 'src/app/base-page/base-page';
 
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.scss'],
 })
-export class MyCoursesComponent  implements OnInit {
+export class MyCoursesComponent extends BasePage implements OnInit {
+  user;
+  search = '';
+  page = 1;
+  last_page = -1;
+  list: any[] = [];
+  count;
+  course;
+  status;
+  categoryId;
+  constructor(injector: Injector) {
+    super(injector)
+    this.getCourses()
+  }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {}
+  async getCourses(search = '', page = 1) {
 
+    return new Promise(async resolve => {
+      let obj = {
+        search: search,
+        page: page
+      }
+
+      if (this.categoryId) {
+        obj['category_id'] = this.categoryId
+      }
+
+
+      const res = this.categoryId ? await this.network.getOtherCourseList(obj) as any : await this.network.getMyCourseList(obj) as any;
+      console.log(res)
+      const result = res.result;
+      this.count = res.result.total
+      this.page = result.current_page;
+      this.last_page = result.last_page;
+      if (this.page == 1) {
+        this.list = result["data"];
+      } else {
+        this.list = [...this.list, ...result["data"]]
+      }
+
+      resolve(true)
+    })
+
+
+  }
 }
