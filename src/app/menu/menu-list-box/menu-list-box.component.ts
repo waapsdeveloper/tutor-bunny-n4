998d48872c4f78f14d021a25590a29637edc3b13
waapsdeveloper <124657993+HamzaShahidWaaps@users.noreply.github.com>
2024-07-18 15:58:31 +0500

    gdf

--- src/app/menu/menu-list-box/menu-list-box.component.ts
+++ src/app/menu/menu-list-box/menu-list-box.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BasePage } from 'src/app/base-page/base-page';
 
 @Component({
   selector: 'app-menu-list-box',
   templateUrl: './menu-list-box.component.html',
   styleUrls: ['./menu-list-box.component.scss'],
 })
-export class MenuListBoxComponent  implements OnInit {
+export class MenuListBoxComponent extends BasePage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {}
+  constructor(injector: Injector) {
+    super(injector)
+  }
+
+  ngOnInit() { }
+
+  gotoProfile(){
+    this.nav.push('/tabs/teacher-profile');
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.nav.push('splash')
+  }
 
 }
