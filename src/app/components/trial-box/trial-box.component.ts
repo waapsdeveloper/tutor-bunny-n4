6c4c8a59a9dbc3c922ced823ddb97ff9ba55055c
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-07-25 14:07:41 +0500

    list data

--- src/app/components/trial-box/trial-box.component.ts
+++ src/app/components/trial-box/trial-box.component.ts
@@ -7,7 +7,7 @@ import { GlobalTrialsService } from 'src/app/services/global-trials.service';
   templateUrl: './trial-box.component.html',
   styleUrls: ['./trial-box.component.scss'],
 })
-export class TrialBoxComponent extends BasePage implements OnInit {
+export class TrialBoxComponent extends BasePage { //  implements OnInit
   user;
   trial;
   student;
@@ -23,61 +23,44 @@ export class TrialBoxComponent extends BasePage implements OnInit {
   serial_number;
   constructor(injector: Injector, public globalTrials: GlobalTrialsService) {
     super(injector)
-    this.initialize();
+    // this.initialize();
   }
-  ngOnInit() {
-    this.trialsReceivedViaPusher();
-  };
-  trialsReceivedViaPusher() {
-    this.events.registerPusherEvent(this.user.id);
+  // ngOnInit() {
+  //   this.trialsReceivedViaPusher();
+  // };
+  // trialsReceivedViaPusher() {
+  //   this.events.registerPusherEvent(this.user.id);
 
-    this.events.subscribe('trials-received-via-pusher', this.updateTrailsList.bind(this));
-  }
+  //   this.events.subscribe('trials-received-via-pusher', this.updateTrailsList.bind(this));
+  // }
 
-  async updateTrailsList(data: any) {
-    let trail_Id = data.id;
+  // async updateTrailsList(data: any) {
+  //   let trail_Id = data.id;
 
-    // return
-    this.initialize();
+  //   // return
+  //   this.initialize();
 
-    this.events.publish('get-dashboard-stats');
+  //   this.events.publish('get-dashboard-stats');
 
-    this.newTrial = await this.network.geTrailRequestsByPusher(trail_Id);
+  //   this.newTrial = await this.network.geTrailRequestsByPusher(trail_Id);
 
-    if (this.newTrial) {
-      const index = this.trial.findIndex(c => c.id === this.newTrial.id);
-      if (index !== -1) {
-        this.list[index] = this.newTrial;
-      } else {
-        this.list = [this.newTrial, ...this.trial];
-      }
-    }
+  //   if (this.newTrial) {
+  //     const index = this.trial.findIndex(c => c.id === this.newTrial.id);
+  //     if (index !== -1) {
+  //       this.list[index] = this.newTrial;
+  //     } else {
+  //       this.list = [this.newTrial, ...this.trial];
+  //     }
+  //   }
 
-  }
+  // }
+
   async initialize() {
     this.globalTrials.getPendingTrials();
   }
-  getFlag(item) {
 
-    if (item && item.student && item.student.student.country.iso2) {
-      const flag = item.student.student.country.iso2;
-
-      if (flag) {
-        return flag.toLowerCase();
-      } else {
-        return ""
-      }
-    } else {
-      return ""
-    }
-  }
   goToTrialReq() {
     this.nav.push('my-students')
   }
-
-  goToChat() {
-    this.nav.push('/tabs/chat')
-  }
-
 
 }
