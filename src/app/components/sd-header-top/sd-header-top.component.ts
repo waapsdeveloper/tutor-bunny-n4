c546a0ac19e5c27765e4dba7d921056ff78a8183
shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-09-14 15:30:59 +0500

    fsd

--- src/app/components/sd-header-top/sd-header-top.component.ts
+++ src/app/components/sd-header-top/sd-header-top.component.ts
@@ -26,6 +26,7 @@ export class SdHeaderTopComponent extends BasePage implements OnInit {
   @Output('parentEdit') parentEdit: EventEmitter<any> = new EventEmitter<any>();
   @Output('addToFav') addToFav: EventEmitter<any> = new EventEmitter<any>();
   @Output('removetoFav') removetoFav: EventEmitter<any> = new EventEmitter<any>();
+  @Output('toggleSearchBar') toggleSearchBar: EventEmitter<any> = new EventEmitter<any>();
 
 
   constructor(injector: Injector) {
@@ -42,7 +43,7 @@ export class SdHeaderTopComponent extends BasePage implements OnInit {
 
     if (this.parentHandleBack) {
       $event.stopImmediatePropagation(); // Stop the event from propagating to other handlers
-      $event.preventDefault(); 
+      $event.preventDefault();
       $event.stopPropagation();
       this.parentBack.emit();
     } else {
@@ -68,6 +69,12 @@ export class SdHeaderTopComponent extends BasePage implements OnInit {
   removeToFav() {
 
     this.removetoFav.emit()
+
+  }
+
+  showSearchBar(){
+
+    this.toggleSearchBar.emit()
 
   }
 }
