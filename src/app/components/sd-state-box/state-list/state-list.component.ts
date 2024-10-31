fddb248dbffdd2e96fae60d367fdeb798b760da2
shoaib uddin <hamzashahid.waaps@gmail.com>
2024-10-31 15:49:54 +0500

    gfd

--- src/app/components/sd-state-box/state-list/state-list.component.ts
+++ src/app/components/sd-state-box/state-list/state-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/basic/modal.service';
 import { NetworkService } from 'src/app/services/network.service';
+import { CountrySqService } from 'src/app/services/sqlite/countries-sq.service';
 import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
@@ -11,7 +12,9 @@ import { UtilityService } from 'src/app/services/utility.service';
 export class StateListComponent  implements OnInit {
   list = [];
   search: "";
-  page = 1;
+  limit = 30;
+  offset = 0;
+
   state;
 
   private _countryId;
@@ -27,42 +30,42 @@ export class StateListComponent  implements OnInit {
 
   }
 
-
-
-
-
-  constructor(private modals: ModalService, private network: NetworkService, private utility: UtilityService) {
+  constructor(private modals: ModalService, private network: NetworkService,private countryService: CountrySqService, private utility: UtilityService) {
 
   }
 
   ngOnInit() {
-    // this.initialize();
+    ;
   }
 
   async initialize() {
     this.search = "";
-    this.page = 1;
+    this.offset = 1;
     this.callApi();
   }
   callApi() {
     return new Promise(async resolve => {
+      console.log(this.countryId);
 
 
-      let obj = {
-        search: this.search,
-        page: this.page,
-        countryId: this.countryId
-      }
+      let rows = await this.countryService.stateList(this.countryId ,this.search, this.offset, this.limit) as any[];
+      console.log(rows);
 
-
-      this.state = await this.network.getStates(obj) as any[];
-      this.page = this.state["current_page"];
-      if (this.page == 1) {
-        this.list = this.state["data"];
-
+      if (this.offset == 0) {
+        this.list = rows;
       } else {
-        this.list = [...this.list, ...this.state["data"]]
+        this.list = [...this.list, ...rows]
       }
+      resolve(true);
+
+      // this.state = await this.network.getStates(obj) as any[];
+      // this.offset = this.state["current_page"];
+      // if (this.offset == 1) {
+      //   this.list = this.state["data"];
+
+      // } else {
+      //   this.list = [...this.list, ...this.state["data"]]
+      // }
       resolve(true);
     })
   }
@@ -71,7 +74,7 @@ export class StateListComponent  implements OnInit {
     this.modals.dismiss()
   }
   async loadMore($event) {
-    this.page = this.state.current_page + 1;
+    this.offset = this.state.current_page + 1;
     await this.callApi();
     $event.target.complete();
   }
@@ -83,7 +86,7 @@ export class StateListComponent  implements OnInit {
   handleInput(event) {
     const query = event.target.value.toLowerCase();
     this.search = query;
-    this.page = 1;
+    this.offset = 1;
     this.callApi();
 
     // this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
--- src/app/services/sqlite/countries-sq.service.ts
+++ src/app/services/sqlite/countries-sq.service.ts
@@ -5,7 +5,6 @@ import { StorageService } from './storage.service';
   providedIn: 'root',
 })
 export class CountrySqService {
-
   countries = [];
 
   constructor(private storageService: StorageService) {}
@@ -15,7 +14,7 @@ export class CountrySqService {
 
     // Add a WHERE clause if a search term is provided
     if (search) {
-        sql += ` WHERE name LIKE ?`;
+      sql += ` WHERE name LIKE ?`;
     }
 
     // Add ORDER BY, OFFSET, and LIMIT clauses
@@ -26,12 +25,45 @@ export class CountrySqService {
 
     // Execute the query and get results
     try {
+      const res = await this.storageService.executeQuery(sql, params);
+      return res; // Assuming res.rows returns the list of rows
+    } catch (error) {
+      console.error('Error executing list query:', error);
+      return [];
+    }
+  }
+
+  async stateList(country_id, search = '', offset = 0, limit = 10) {
+    console.log(country_id);
+
+    let sql = `SELECT * FROM states`;
+    const params = [];
+
+    // Add a WHERE clause if a country_id or search term is provided
+    if (country_id) {
+        sql += ` WHERE country_id = ?`;
+        params.push(country_id);
+    }
+
+    if (search) {
+        sql += country_id ? ` AND` : ` WHERE`;
+        sql += ` name LIKE ?`;
+        params.push(`%${search}%`);
+    }
+
+    // Add ORDER BY, OFFSET, and LIMIT clauses
+    sql += ` ORDER BY name ASC LIMIT ? OFFSET ?`;
+    params.push(limit, offset);
+
+    // Execute the query and get results
+    try {
         const res = await this.storageService.executeQuery(sql, params);
         return res; // Assuming res.rows returns the list of rows
     } catch (error) {
-        console.error("Error executing list query:", error);
+        console.error('Error executing list query:', error);
         return [];
     }
 }
+
 
 }
