22befc601dc37a2c58aa03faace9956e129ece64
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-16 16:21:36 +0500

    Updates till app-course-teacher-study-card

---
+++ src/app/components/course-teacher-study-card/course-teacher-study-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-teacher-study-card',
+  templateUrl: './course-teacher-study-card.component.html',
+  styleUrls: ['./course-teacher-study-card.component.scss'],
+})
+export class CourseTeacherStudyCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
