0cedc89c3c96151c6cb341e46dd651c5b34a9a59
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2024-12-20 22:20:31 +0500

    dashboard

--- src/app/components/statistic-box/statistic-box.module.ts
+++ src/app/components/statistic-box/statistic-box.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticBoxComponent } from './statistic-box.component';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
   declarations: [StatisticBoxComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    IonicModule
   ],
   exports: [
     StatisticBoxComponent
