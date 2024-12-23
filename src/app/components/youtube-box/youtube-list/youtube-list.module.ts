882506ae590bf00c53c9fecaeb01a5a2b773b200
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2024-12-23 17:05:17 +0500

    youtube updates

--- src/app/components/youtube-box/youtube-list/youtube-list.module.ts
+++ src/app/components/youtube-box/youtube-list/youtube-list.module.ts
@@ -1,13 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YoutubeListComponent } from './youtube-list.component';
-
+import { SwiperModule } from 'swiper/angular';
 
 
 @NgModule({
   declarations: [YoutubeListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule
   ],
   exports:[YoutubeListComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
