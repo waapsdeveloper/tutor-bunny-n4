18e6ec418c7a4814116c2315e1d76d1f3fa95309
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2024-12-31 14:27:46 +0500

    detail material page

--- src/app/pages/study-material/pages/detail-material/detail-material.module.ts
+++ src/app/pages/study-material/pages/detail-material/detail-material.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { OtherCoursesModule } from 'src/app/components/other-courses/other-courses.module';
 import { IonicModule } from '@ionic/angular';
 
 import { DetailMaterialPageRoutingModule } from './detail-material-routing.module';
 
 import { DetailMaterialPage } from './detail-material.page';
+import { CourseSchedulesModule } from 'src/app/components/course-schedules/course-schedules.module';
+import { CoursePhotoModule } from 'src/app/components/course-photo/course-photo.module';
+import { RatingStarsModule } from 'src/app/components/rating-stars/rating-stars.module';
+import { CoursePhotosModule } from 'src/app/components/course-photos/course-photos.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DetailMaterialPageRoutingModule
+    DetailMaterialPageRoutingModule,
+    OtherCoursesModule,
+    CourseSchedulesModule,
+    CoursePhotosModule,
+    RatingStarsModule
+
   ],
   declarations: [DetailMaterialPage]
 })
