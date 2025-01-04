997c62e8e44644018603a73645c25dd107d1ffaa
Ali Murtaza <murtazarizvi407@gmail.com>
2025-01-04 16:48:56 +0500

    Favmaterial

--- src/app/services/global-fav-material.service.ts
+++ src/app/services/global-fav-material.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
 
+
+import {
+  NgSimpleStateBaseRxjsStore,
+  NgSimpleStateStoreConfig,
+} from 'ng-simple-state';
+import { UsersService } from './users.service';
+import { NetworkService } from './network.service';
+
+export interface GlobalFavMaterailModel {
+  user_id: number;
+  course_id: number;
+
+}
+export type GlobalFavMaterailModelState = Array<GlobalFavMaterailModel>;
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalFavMaterialService {
+export class GlobalFavMaterialService extends NgSimpleStateBaseRxjsStore< GlobalFavMaterailModelState >{
 
-  constructor() { }
-}
+  page = 1;
+  last_page = -1;
+  constructor(private users: UsersService, private network: NetworkService) {
+    super();
+  }
+  storeConfig(): NgSimpleStateStoreConfig {
+      return {
+        storeName: 'GlobalFavMaterailModel',
+      };
+    }
+
+    initialState(): GlobalFavMaterailModelState {
+      return [];
+    }
+
+    getList() {
+      return this.selectState((state) => state);
+    }
+
+    getCount() {
+      return this.selectState((state) => state.length);
+    }
+
+    getCountPromise() {
+      return new Promise((resolve) => {
+        this.selectState((state) => state.length).subscribe((res) => {
+          resolve(res);
+        });
+      });
+    }
+  }
+
+
