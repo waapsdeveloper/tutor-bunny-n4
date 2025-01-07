a6c6533e1d85a88fb25dfeac7cf2e97ce344c8d4
Wajih-09 <wajihwork0902@gmail.com>
2025-01-07 16:43:52 +0500

    material details

--- src/app/pages/study-material/pages/detail-material/material-attachments/material-attachments.component.ts
+++ src/app/pages/study-material/pages/detail-material/material-attachments/material-attachments.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Injector,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { BasePage } from 'src/app/base-page/base-page';
 
 @Component({
   selector: 'app-material-attachments',
   templateUrl: './material-attachments.component.html',
   styleUrls: ['./material-attachments.component.scss'],
 })
-export class MaterialAttachmentsComponent  implements OnInit {
 
-  constructor() { }
+export class MaterialAttachmentsComponent extends BasePage implements OnInit {
+  @Input() count = 0;
+  @Output() openOtherCourses = new EventEmitter<any>();
+  @Output('onChange') onChange: EventEmitter<any> = new EventEmitter<any>();
 
-  ngOnInit() {}
+  private _list;
+  @Input()
+  public get list(): any[] {
+    return this._list;
+  }
 
+  public set list(value: any[]) {
+    this._list = value;
+  }
+
+  constructor(injector: Injector) {
+    super(injector);
+  }
+
+  ngOnInit() {
+
+  }
+
+  gotoCourseList() {
+    this.openOtherCourses.emit();
+  }
+
+  getOtherCourse(events) {
+
+    this.onChange.emit(events);
+  }
 }
