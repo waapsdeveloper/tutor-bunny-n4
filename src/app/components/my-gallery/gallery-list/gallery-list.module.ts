f0592136d612233346115f43b5223077c6988128
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-01-14 13:06:28 +0500

    swiper added in gallery list

--- src/app/components/my-gallery/gallery-list/gallery-list.module.ts
+++ src/app/components/my-gallery/gallery-list/gallery-list.module.ts
@@ -1,13 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryListComponent } from './gallery-list.component';
-
+import { SwiperModule } from 'swiper/angular';
 
 
 @NgModule({
   declarations: [GalleryListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule
   ],
   exports:[GalleryListComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
