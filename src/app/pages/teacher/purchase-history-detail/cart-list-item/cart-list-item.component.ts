556c87c14f91a994a5ccf82f00333c6242fd18f9
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-01-27 16:37:32 +0500

    purchase list item updated

---
+++ src/app/pages/teacher/purchase-history-detail/cart-list-item/cart-list-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-list-item',
+  templateUrl: './cart-list-item.component.html',
+  styleUrls: ['./cart-list-item.component.scss'],
+})
+export class CartListItemComponent {
+
+  @Output() openDetails = new EventEmitter<any>()
+  @Output() removeCartitem = new EventEmitter<any>()
+
+  private _item: any;
+
+  @Input()
+  get item(){
+    return this._item;
+  }
+
+  set item(value: any){
+    this._item = value;
+  }
+
+  constructor() { }
+
+  initialize(){
+    
+  }
+
+}
