1752e7875071d174726e0c292f84691a7336c124
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-01-28 14:46:29 +0500

    update

--- src/app/services/global-study-material.service.ts
+++ src/app/services/global-study-material.service.ts
@@ -1,80 +1,24 @@
 import { Injectable } from '@angular/core';
-
-import {
-  NgSimpleStateBaseRxjsStore,
-  NgSimpleStateStoreConfig,
-} from 'ng-simple-state';
 import { UsersService } from './users.service';
 import { NetworkService } from './network.service';
-
-
-export interface GlobalStudyMaterialModel {
-    id: number
-    is_liked_by_me: boolean,
-    user_id: 57,
-    user: any,
-    title: string,
-    description: string,
-    language_id: number,
-    image: string,
-    price: string,
-    keywords: [],
-    auth_user_currency_symbol: string,
-    updated_price: string
-}
-
-export type GlobalStudyMaterialModelState = Array<GlobalStudyMaterialModel>;
+import { NgrxCrudService } from './abstract/ngrx-crud.service';
+import Pusher from 'pusher-js';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalStudyMaterialService extends NgSimpleStateBaseRxjsStore<GlobalStudyMaterialModelState> {
+export class GlobalStudyMaterialService extends NgrxCrudService<any> {
 
 
-  page = 1;
-  last_page = -1;
+  trialChannel: any;
+  private pusher: Pusher;
+
+  ngrxModelName: string = 'GlobalStudyMaterialModel';
 
   constructor(private users: UsersService, private network: NetworkService) {
     super();
   }
 
-  storeConfig(): NgSimpleStateStoreConfig {
-    return {
-      storeName: 'GlobalStudyMaterialModel',
-    };
-  }
-
-  initialState(): GlobalStudyMaterialModelState {
-    return [];
-  }
-
-  getList() {
-    return this.selectState((state) => state);
-  }
-
-  getItem(id) {
-    return this.selectState((state) => state.find((x) => x.id == id));
-  }
-
-  getItemPromise(id) {
-    return new Promise((resolve) => {
-      this.selectState((state) => state.find((x) => x.id == id)).subscribe((res) => {
-        resolve(res);
-      });
-    });
-  }
-
-  getCount() {
-    return this.selectState((state) => state.length);
-  }
-
-  getCountPromise() {
-    return new Promise((resolve) => {
-      this.selectState((state) => state.length).subscribe((res) => {
-        resolve(res);
-      });
-    });
-  }
 
   getGlobalStudyMaterialFromApi(search = '', page = 1) {
     return new Promise(async (resolve) => {
@@ -89,15 +33,7 @@ export class GlobalStudyMaterialService extends NgSimpleStateBaseRxjsStore<Globa
 
 
       const data = res.result;
-      this.page = data.current_page;
-      this.last_page = data.last_page;
-
-      this.setState( (state) => {
-        if (page === 1) {
-          return data.data;
-        }
-        return [...state, ...data.data];
-      });
+      this.setList(data.data, data.page, data.last_page, data.total);
 
       resolve(true);
     });
@@ -115,15 +51,7 @@ export class GlobalStudyMaterialService extends NgSimpleStateBaseRxjsStore<Globa
       let res = await this.network.getMyMaterialList(obj, user.id);
 
       const data = res.result;
-      this.page = data.current_page;
-      this.last_page = data.last_page;
-
-      this.setState( (state) => {
-        if (page === 1) {
-          return data.data;
-        }
-        return [...state, ...data.data];
-      });
+      this.setList(data.data, data.page, data.last_page, data.total);
 
       resolve(true);
     });
