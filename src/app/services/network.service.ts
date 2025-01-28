beff1bd09d5a84ddab816890a7dfd27b0b65991c
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-01-28 14:36:20 +0500

    in between changes profile video

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -831,6 +831,17 @@ export class NetworkService {
     );
   }
 
+  uploadIntoVideoFile(data) {
+    return this.httpPostResponse(
+      'profile/video',
+      data,
+      null,
+      false,
+      false,
+      'multipart/form-data'
+    );
+  }
+
   deleteStudyMaterialFile(data) {
     return this.httpPostResponse(
       'material/delete/document',
