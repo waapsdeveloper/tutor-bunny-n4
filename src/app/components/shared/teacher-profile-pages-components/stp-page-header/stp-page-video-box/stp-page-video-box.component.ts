cd319afefea7b08f47e8289627306ac5e6352cf4
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2025-01-30 22:53:06 +0500

    updates

--- src/app/components/shared/teacher-profile-pages-components/stp-page-header/stp-page-video-box/stp-page-video-box.component.ts
+++ src/app/components/shared/teacher-profile-pages-components/stp-page-header/stp-page-video-box/stp-page-video-box.component.ts
@@ -6,23 +6,34 @@ import { NetworkService } from 'src/app/services/network.service';
   templateUrl: './stp-page-video-box.component.html',
   styleUrls: ['./stp-page-video-box.component.scss'],
 })
-export class StpPageVideoBoxComponent implements OnInit {
-  private;
-  constructor(private network: NetworkService) {}
+export class StpPageVideoBoxComponent {
+  videoUrl: string | null = null;
+
+  constructor(private network: NetworkService) { }
+
+
   private _data: any;
+
+
   @Input()
   set data(value: any) {
     this._data = value;
+    this.setData(value)
   }
+
 
   get data(): any {
     return this._data;
   }
 
-    async ngOnInit() {
-      console.log(this.data);
-      let res = await this.network.getIntoVideoFile(this.data);
-      console.log(res);
+  async setData(value: any) {
+    let res = await this.network.getIntoVideoFile(value)
+    console.log(res);
+    if (res?.result?.full_url) {
+      this.videoUrl = res.result.full_url;
+      console.log(this.videoUrl);
+
     }
+  }
 
 }
