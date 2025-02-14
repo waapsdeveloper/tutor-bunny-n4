9aaffe923140bcf29560c9b2e95f86290bac37c5
Shoaib uddin <99804964+waapsdeveloper@users.noreply.github.com>
2025-02-14 21:49:31 +0500

    service udpate

--- src/app/pages/student/search-box/search-filter.service.ts
+++ src/app/pages/student/search-box/search-filter.service.ts
@@ -1,118 +1,229 @@
 import { Injectable } from '@angular/core';
 import { NetworkService } from '../../../services/network.service';
 
+import {
+  NgSimpleStateBaseRxjsStore,
+  NgSimpleStateStoreConfig,
+} from 'ng-simple-state'; // Adjust imports as needed
+
+export interface GlobalSearchFormModel {
+  search: string;
+  keywords: string;
+  language: string;
+  mode_type: string;
+  capacity: string;
+  price: string;
+  name: string;
+  country: string;
+  from_age: string;
+  to_age: string;
+  keyword_id: string;
+  hourly_rate: string;
+  country_id: string;
+  travel_policy_id: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class SearchFilterService {
-
-  private selectedKeyword: any = null;
-  searchText: any = null;
+export class SearchFilterService extends NgSimpleStateBaseRxjsStore<GlobalSearchFormModel> {
 
 
+  ngrxModelName: string = 'GlobalSearchFormModel';
 
-
-  formData = {
-    search: null,
-    keywords: null,
-    language: null,
-    mode_type: null,
-    capacity: null,
-    price: null,
-    name: null,
-    country: null,
-    from_age: null,
-    to_age: null,
-    keyword_id: null,
-    hourly_rate: null,
-    country_id: null,
-    travel_policy_id: null,
-  };
-  searchList: any[] = [];
-  searchTitals: any[] = [];
-  countryId = null;
-  stateId = null;
-  keywords;
-  searchCourses;
-  page = 0;
-  last_page = -1;
-  constructor(private network: NetworkService) {}
-
-
-
-  getSelectedKeyword() {
-    return this.selectedKeyword;
-  }
-  setSelectedKeyword(keyword) {
-    this.selectedKeyword = keyword;
+  storeConfig(): NgSimpleStateStoreConfig {
+    return {
+      storeName: this.ngrxModelName,
+    };
   }
 
-
-
-
-  updateFormData(value: any, key: string): void {
-    this.formData[key] = value;
-    if (key == 'keyword' && value && value.length > 0) {
-      this.formData['keyword_id'] = value[0].id;
-      this.formData['keywords'] = value;
-    }
+  initialState(): GlobalSearchFormModel {
+    return {
+      search: '',
+      keywords: '',
+      language: '',
+      mode_type: '',
+      capacity: '',
+      price: '',
+      name: '',
+      country: '',
+      from_age: '',
+      to_age: '',
+      keyword_id: '',
+      hourly_rate: '',
+      country_id: '',
+      travel_policy_id: '',
+    };
   }
 
-  getFormData(): any {
-    return this.formData;
+  constructor(private network: NetworkService) {
+    super()
   }
 
-  async submitFormData(page): Promise<any> {
+  getFormData(){
+    return this.selectState((state) => state );
+  }
 
-      let obj = {
-        search: '',
-        page: page,
-        liked: false,
-        language_id: this.formData.language,
-        price: this.formData.price,
-        mode: this.formData.mode_type,
-        capacity: this.formData.capacity,
-        hourly_rate: this.formData.hourly_rate,
-        teacher_name: this.formData.name,
-        country_id: this.formData.country,
-        travel_policy_id: this.formData.travel_policy_id,
-        from_age: this.formData.from_age,
-        to_age: this.formData.to_age,
-        keyword_id: this.formData.keyword_id,
-        keyword: this.formData.keywords,
+  setSearch(search: string){
+    this.setState((state) => {
+      return {
+        ...state,
+        search: search,
       };
-      return obj;
-
-      // const res = (await this.network.getAllCourses(obj)) as any;
-      // const data = res.result;
-
-      // this.page = data.current_page;
-      // this.last_page = data.last_page;
-      // if (page === 1) {
-      //   this.searchList = data.data;
-      // } else {
-      //   this.searchList = [...this.searchList, ...data.data];
-      // }
-
-    // });
-    // Add additional logic for submission if needed.
+    });
   }
 
-  setCountryId(countryId: string): void {
-    this.countryId = countryId;
+  setKeywords(keywords: string) {
+    this.setState((state) => ({
+      ...state,
+      keywords: keywords,
+    }));
+  }
+  
+  setLanguage(language: string) {
+    this.setState((state) => ({
+      ...state,
+      language: language,
+    }));
+  }
+  
+  setModeType(mode_type: string) {
+    this.setState((state) => ({
+      ...state,
+      mode_type: mode_type,
+    }));
+  }
+  
+  setCapacity(capacity: string) {
+    this.setState((state) => ({
+      ...state,
+      capacity: capacity,
+    }));
+  }
+  
+  setPrice(price: string) {
+    this.setState((state) => ({
+      ...state,
+      price: price,
+    }));
+  }
+  
+  setName(name: string) {
+    this.setState((state) => ({
+      ...state,
+      name: name,
+    }));
+  }
+  
+  setCountry(country: string) {
+    this.setState((state) => ({
+      ...state,
+      country: country,
+    }));
+  }
+  
+  setFromAge(from_age: string) {
+    this.setState((state) => ({
+      ...state,
+      from_age: from_age,
+    }));
+  }
+  
+  setToAge(to_age: string) {
+    this.setState((state) => ({
+      ...state,
+      to_age: to_age,
+    }));
+  }
+  
+  setKeywordId(keyword_id: string) {
+    this.setState((state) => ({
+      ...state,
+      keyword_id: keyword_id,
+    }));
+  }
+  
+  setHourlyRate(hourly_rate: string) {
+    this.setState((state) => ({
+      ...state,
+      hourly_rate: hourly_rate,
+    }));
+  }
+  
+  setCountryId(country_id: string) {
+    this.setState((state) => ({
+      ...state,
+      country_id: country_id,
+    }));
+  }
+  
+  setTravelPolicyId(travel_policy_id: string) {
+    this.setState((state) => ({
+      ...state,
+      travel_policy_id: travel_policy_id,
+    }));
+  }
+  
+
+  getSearch() {
+    return this.selectState((state) => state.search);
+  }
+  
+  getKeywords() {
+    return this.selectState((state) => state.keywords);
+  }
+  
+  getLanguage() {
+    return this.selectState((state) => state.language);
+  }
+  
+  getModeType() {
+    return this.selectState((state) => state.mode_type);
+  }
+  
+  getCapacity() {
+    return this.selectState((state) => state.capacity);
+  }
+  
+  getPrice() {
+    return this.selectState((state) => state.price);
+  }
+  
+  getName() {
+    return this.selectState((state) => state.name);
+  }
+  
+  getCountry() {
+    return this.selectState((state) => state.country);
+  }
+  
+  getFromAge() {
+    return this.selectState((state) => state.from_age);
+  }
+  
+  getToAge() {
+    return this.selectState((state) => state.to_age);
+  }
+  
+  getKeywordId() {
+    return this.selectState((state) => state.keyword_id);
+  }
+  
+  getHourlyRate() {
+    return this.selectState((state) => state.hourly_rate);
+  }
+  
+  getCountryId() {
+    return this.selectState((state) => state.country_id);
+  }
+  
+  getTravelPolicyId() {
+    return this.selectState((state) => state.travel_policy_id);
   }
 
-  getCountryId(): string | null {
-    return this.countryId;
-  }
 
-  setStateId(stateId: string): void {
-    this.stateId = stateId;
-  }
-
-  getStateId(): string | null {
-    return this.stateId;
-  }
+  
 
 
+  
 }
