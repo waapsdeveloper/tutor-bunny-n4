4d75fd46be01690a7b9032d0d19ee8945c5d4d56
Muhammad Shayan Bhatti <shayanbhatti332@gmail.com>
2025-02-14 21:50:07 +0500

    lk

--- src/app/pages/student/search-box/pages/student-dashborad-courses/student-dashborad-courses.module.ts
+++ src/app/pages/student/search-box/pages/student-dashborad-courses/student-dashborad-courses.module.ts
@@ -5,7 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { StudentDashboradCoursesPageRoutingModule } from './student-dashborad-courses-routing.module';
 import { StudentDashboradCoursesPage } from './student-dashborad-courses.page';
 import { GlobalListViewModule } from 'src/app/components/global-list-view/global-list-view.module';
-import { GenericCourseCardModule } from 'src/app/components/generic-course-card/generic-course-card.module';
+import { GenericCourseCardModule } from '../../../components/generic-course-card/generic-course-card.module';
 
 @NgModule({
   imports: [
