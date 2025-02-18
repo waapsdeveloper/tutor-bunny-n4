60590dc73006cb14559a8728990c94575bdaeabb
Wajih-09 <wajihwork0902@gmail.com>
2025-02-18 18:11:09 +0500

    keyword button

--- src/app/components/search-keyword/keyword-list/keyword-list.component.ts
+++ src/app/components/search-keyword/keyword-list/keyword-list.component.ts
@@ -98,8 +98,10 @@ export class KeywordListComponent implements OnInit {
       };
       const res2 = await this.network.getMyKeyword(data);
       this.inputText = '';
+
       this.subs = res2.result;
       this.suggestionsList = [];
+
       this.onChange.emit({
         subs: this.subs,
       });
